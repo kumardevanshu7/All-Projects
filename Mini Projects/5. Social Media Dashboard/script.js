5fa9dd331e64c5471151dd7174948a38e73d982c
Kumar Devanshu <devanshukd007@gmail.com>
2023-11-27 18:19:13 +0530

    main files

--- Mini Projects/5. Social Media Dashboard/script.js
+++ Mini Projects/5. Social Media Dashboard/script.js
@@ -1,12 +1,12 @@
 let checkbox = document.querySelector("input[name=theme]");
 
-checkbox.addEventListener("change", function(){
-  
-    if(this.checked){
+checkbox.addEventListener("change", function () {
+
+    if (this.checked) {
         trans();
         document.documentElement.setAttribute("data-theme", "dark");
     }
-    else{
+    else {
         trans();
         document.documentElement.setAttribute("data-theme", "light");
     }
@@ -16,7 +16,7 @@ checkbox.addEventListener("change", function(){
 let trans = () => {
     document.documentElement.classList.add("transition");
 
-    window.setTimeout(()=>{
+    window.setTimeout(() => {
         document.documentElement.classList.remove("transition")
     }, 1000)
 }
